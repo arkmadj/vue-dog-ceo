f7fd69da00aaa7ace6233fb9ededd71d863a6ed4
arkmadj <ahmadjinadu@gmail.com>
2023-02-07 23:46:11 +0100

    Added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="flex justify-between h-28 items-center">
+		<div class="flex">
+			<span class="font-titan text-[#222222]">dogfinder</span>
+			<img
+				alt="Vue logo"
+				class="logo"
+				src="@/assets/logo.svg"
+				width="18"
+				height="18"
+			/>
+		</div>
+
+		<nav>
+			<RouterLink
+				to="/"
+				class="font-poppins text-sm font-medium text-black hover:text-[#624CAB]"
+				>Contact</RouterLink
+			>
+		</nav>
+	</header>
+</template>
