62549fb6bfffb9e6306b9cded892dc8816d96501
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 00:05:04 +0100

    Added unbounded font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-      fontFamily: {
-        'titan': ['Titan One', 'cursive'],
-        'poppins': ['Poppins', 'sans-serif']
-      }
-    },
+			fontFamily: {
+				titan: ["Titan One", "cursive"],
+				poppins: ["Poppins", "sans-serif"],
+				unbounded: ["Unbounded", "cursive"],
+			},
+		},
 	},
 	plugins: [],
 };
