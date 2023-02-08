bf76d8810a2a35f52ba2137532f241179b8b3394
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 03:14:54 +0100

    Added DogCards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,23 @@
 <script setup>
 import HeroSection from "../components/HeroSection.vue";
 import TabNav from "../components/TabNav.vue";
-
+import DogCard from "../components/DogCard.vue";
 </script>
 
 <template>
-  <main>
-    <HeroSection />
-    <TabNav/>
-  </main>
+	<main>
+		<HeroSection />
+		<TabNav />
+		<div class="grid grid-cols-4 gap-x-[26px] gap-y-[50px]">
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+			<DogCard />
+		</div>
+	</main>
 </template>
