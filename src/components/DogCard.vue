02e552963f0a6a287bc17c51a58b722dfc79b50d
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 23:53:52 +0100

    Added url prop to dog card

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -3,13 +3,28 @@
 		<img
 			class="object-cover rounded-2xl h-44 w-72 border-2 border-[#624CAB]"
 			alt="Dog image"
-			src="@/assets/images/dog-image.jpeg"
+			:src="props.url"
 			width="279"
 			height="174"
 		/>
 		<div class="flex items-center justify-between px-3 h-14">
 			<spa class="">Beagle</spa>
-			<span class="bg-[#DCD4F7] rounded-lg text-[#534091] text-xs px-[10px] py-1 font-bold">Hound</span>
+			<span
+				class="bg-[#DCD4F7] rounded-lg text-[#534091] text-xs px-[10px] py-1 font-bold capitalize"
+				>{{ breed }}</span
+			>
 		</div>
 	</div>
 </template>
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+	url: {
+		type: String,
+		required: true,
+	},
+});
+
+const breed = computed(() => props.url.split("/")[4]);
+const name = computed(() => props.url.split("/")[5]);
+</script>
