6108b6f353650871a70c4e8ee8cdc4a9489b231a
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 23:54:52 +0100

    Fetch all breeds from store

--- src/components/TabNav.vue
+++ src/components/TabNav.vue
@@ -1,19 +1,31 @@
 <template>
-    <nav class="flex gap-12 pb-2 border-b border-b-[#E6E6E6] mb-[60px]">
-      <div class="flex flex-col items-center w-fit">
-        <i>
-          <DogFaceIcon/>
-        </i>
-        <span>All breeds</span>
-      </div>
-      <div class="flex flex-col items-center w-fit order hover:text-[#624CAB]">
-        <i>
-          <DogFaceIcon/>
-        </i>
-        <span>All breeds</span>
-      </div>
-    </nav>
+	<!-- <p>Breeds: {{ breeds }}</p> -->
+	<nav class="flex gap-12 border-b border-b-[#E6E6E6] mb-[60px] overflow-x-hidden">
+		<div
+			:class="`${key === 0 ? 'border-b-[3px] border-b-[#624CAB]' : ''} flex flex-col items-center gap-3 min-w-max`"
+			v-for="(breed, key) in breeds"
+			:key="key"
+		>
+			<i>
+				<DogFaceIcon />
+			</i>
+			<span class="capitalize">{{ breed }}</span>
+		</div>
+	</nav>
 </template>
 <script setup>
-import DogFaceIcon from './icons/DogFaceIcon.vue';
+import { useStore } from "vuex";
+import { computed, onMounted } from "vue";
+import DogFaceIcon from "./icons/DogFaceIcon.vue";
+
+const store = useStore();
+
+const breeds = computed(() => [
+	"All breeds",
+	...Object.keys(store.getters.breeds),
+]);
+
+onMounted(() => {
+	store.dispatch("fetchAllBreeds");
+});
 </script>
