44a9245ec578191653e0053bdd0b8895082b29f5
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 23:53:35 +0100

    Added fetch dogs

--- src/api/dogs.js
+++ src/api/dogs.js
@@ -5,6 +5,12 @@ async function fetchAllBreeds() {
 	return response;
 }
 
+async function fetchDogs(){
+  const response = await axios.get("/breeds/image/random/50")
+  return response
+}
+
 export default {
 	fetchAllBreeds,
+  fetchDogs,
 };
