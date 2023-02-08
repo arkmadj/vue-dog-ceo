894bfe48fb82912929d82e5c0967b2a1d864830a
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 03:14:20 +0100

    Added Dog cards

---
+++ src/components/DogCard.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="bg-[#F1EEFC] h-[235px] rounded-2xl w-[279px]">
+		<img
+			class="object-cover rounded-2xl h-44 w-72 border-2 border-[#624CAB]"
+			alt="Dog image"
+			src="@/assets/images/dog-image.jpeg"
+			width="279"
+			height="174"
+		/>
+		<div class="flex items-center justify-between px-3 h-14">
+			<spa class="">Beagle</spa>
+			<span class="bg-[#DCD4F7] rounded-lg text-[#534091] text-xs px-[10px] py-1 font-bold">Hound</span>
+		</div>
+	</div>
+</template>
