8cf0d6e7c6901a79bf16f82a9143da76ab4f1c10
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 23:54:11 +0100

    Added fetch dogs from store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,29 @@
 import HeroSection from "../components/HeroSection.vue";
 import TabNav from "../components/TabNav.vue";
 import DogCard from "../components/DogCard.vue";
+import { onMounted, computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const dogs = computed(() => store.getters.dogs);
+console.log({ dogs });
+
+onMounted(() => {
+	store.dispatch("fetchDogs");
+});
+
+const lazyLoadImages(){
+}
 </script>
 
 <template>
 	<main>
+		<!-- <p>{{ dogs }}</p> -->
 		<HeroSection />
 		<TabNav />
 		<div class="grid grid-cols-4 gap-x-[26px] gap-y-[50px]">
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
-			<DogCard />
+			<DogCard v-for="(url, key) in dogs" :key="key" :url="url"/>
 		</div>
 	</main>
 </template>
