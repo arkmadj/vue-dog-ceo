419e65a4255784eb87e2b8acb97cfbb757878dd7
arkmadj <ahmadjinadu@gmail.com>
2023-02-08 22:24:40 +0100

    Added slide in animation

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,23 +1,25 @@
 <template>
-	<section class="flex flex-col items-center mb-28">
+	<section class="relative flex flex-col items-center bg-resd-500 mb-28">
 		<h1
-			class="font-unbounded text-[#624CAB] text-5xl w-[544px] text-center mb-5"
+			class="font-unbounded text-[#624CAB] text-5xl w-[544px] text-center mb-5 relative animate-slide-in -translate-y-10"
 		>
 			Find your next best friend
 		</h1>
-		<p class="text-[#222222] font-poppins w-[609px] text-center mb-5">
+		<p class="text-[#222222] font-poppins w-[609px] text-center mb-5 animate-slide-in">
 			We understand that adulting can get lonely. With more than 100 breeds of
 			dogs, learning and choosing the right friend for you has never been
 			easier.
 		</p>
 		<div
-			class="border border-[#828282] rounded-full flex items-center h-14 w-[586px] justify-between pl-6 gap-1 focus-within:border-[#624CAB] focus-within:ring-[#624CAB] focus-within:ring-1"
+			class="border border-[#828282] rounded-full flex items-center h-14 w-[586px] justify-between pl-6 gap-1 focus-within:border-[#624CAB] focus-within:ring-[#624CAB] focus-within:ring-1 animate-slide-in-delay opacity-0"
 		>
 			<i>
 				<SearchIcon />
 			</i>
 			<input class="w-full focus:outline-none" placeholder="Search breed" />
-			<i class="bg-[#624CAB] h-full overflow-hidden w-16 rounded-r-full grid place-items-center">
+			<i
+				class="bg-[#624CAB] h-full overflow-hidden w-16 rounded-r-full grid place-items-center"
+			>
 				<ArrowRightIcon />
 			</i>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
 				poppins: ["Poppins", "sans-serif"],
 				unbounded: ["Unbounded", "cursive"],
 			},
+			keyframes: {
+        'slide-in': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+				}
+			},
+			animation: {
+        'slide-in': 'slide-in 1s ease 0s 1 normal forwards',
+        'slide-in-delay': 'slide-in 1s ease 0.3s 1 normal forwards',
+        // 'spin-slow': 'spin 1s linear infinite',
+			}
 		},
 	},
 	plugins: [],
