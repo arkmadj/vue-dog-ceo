b8af22a9a36f635ce21b4fe80516fafb5f13eb9b
arkmadj <ahmadjinadu@gmail.com>
2023-02-09 17:46:03 +0100

    Added search functionality

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,7 +6,7 @@
 			Find your next best friend
 		</h1>
 		<p
-			class="text-[#222222] font-poppins w-[278px] md:w-[609px] text-center mb-2 md:mb-5 animate-slide-in"
+			class="text-[#222222] font-poppins w-[278px] md:w-[609px] text-center mb-5 animate-slide-in"
 		>
 			We understand that adulting can get lonely
 			<span class="hidden md:inline">
@@ -16,14 +16,14 @@
 			<span class="md:hidden">, find the only friend you'll need for life.</span>
 		</p>
 		<div
-			class="border border-[#828282] rounded-full items-center h-14 w-[586px] justify-between pl-6 gap-1 focus-within:border-[#624CAB] focus-within:ring-[#624CAB] focus-within:ring-1 animate-slide-in-delay opacity-0 md:flex hidden"
+			class="border border-[#828282] rounded-full items-center h-14 md:w-[586px]  w-[286px] justify-between pl-6 gap-1 focus-within:border-[#624CAB] focus-within:ring-[#624CAB] focus-within:ring-1 animate-slide-in-delay opacity-0 flex"
 		>
 			<i>
 				<SearchIcon />
 			</i>
-			<input class="w-full focus:outline-none" placeholder="Search breed" />
+			<input v-model="searchTerm" class="w-full focus:outline-none" placeholder="Search breed" @keypress.enter="handleSearch"/>
 			<i
-				class="bg-[#624CAB] h-full overflow-hidden w-16 rounded-r-full grid place-items-center"
+				class="bg-[#624CAB] h-full overflow-hidden w-16 rounded-r-full grid place-items-center cursor-pointer hover:bg-[#53428a]" @click="handleSearch"
 			>
 				<ArrowRightIcon />
 			</i>
@@ -32,6 +32,15 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import ArrowRightIcon from "./icons/IconArrowRight.vue";
 import SearchIcon from "./icons/IconSearch.vue";
+
+const searchTerm = ref("")
+
+const emit = defineEmits(['search'])
+
+const handleSearch = () => {
+	emit('search', searchTerm.value.toLowerCase())
+}
 </script>
