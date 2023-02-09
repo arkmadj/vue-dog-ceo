cf0f32333e34810e200753442053a5f8c21e3ce1
arkmadj <ahmadjinadu@gmail.com>
2023-02-09 14:54:37 +0100

    Added mobile responsiveness for Dogs card

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="bg-[#F1EEFC] h-[235px] rounded-2xl w-[279px]">
+	<div class="bg-[#F1EEFC] h-[235px] rounded-2xl w-full">
 		<div class="relative">
 			<img
-				class="object-cover rounded-2xl h-44 w-72 border-2 border-[#624CAB] cursor-pointers"
+				class="object-cover rounded-2xl h-44 w-full border-2 border-[#624CAB] cursor-pointers"
 				alt="Dog image"
 				width="279"
 				height="174"
@@ -20,7 +20,10 @@
 			</div>
 		</div>
 		<div class="flex items-center justify-between px-3 h-14">
-			<span class="font-semibold">Beagle</span>
+			<div class="flex flex-col">
+				<span class="text-[10px] text-gray-500 font-base">Sub breed</span>
+				<span class="text-xs font-semibold capitalize">{{ subBreed ?  subBreed : '-' }}</span>
+			</div>
 			<span
 				class="bg-[#DCD4F7] rounded-lg text-[#534091] text-xs px-[10px] py-1 font-bold capitalize"
 				>{{ breed }}</span
@@ -41,6 +44,6 @@ const gotoDogDetails = () => {
 	alert(breed.value);
 };
 
-const breed = computed(() => props.url.split("/")[4]);
-const name = computed(() => props.url.split("/")[5]);
+const breed = computed(() => props.url.split("/")[4]?.split("-")[0]);
+const subBreed = computed(() => props.url.split("/")[4]?.split("-")[1]);
 </script>
