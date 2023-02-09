9d62d5b6af24a93b65a77208c35e35484517a804
arkmadj <ahmadjinadu@gmail.com>
2023-02-09 08:44:01 +0100

    Added lazy loading

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import store from "./store"
 import App from './App.vue'
 import router from './router'
+import VueLazyload from 'vue-lazyload'
 
 import './assets/main.css'
 
 const app = createApp(App)
+app.use(VueLazyload)
 app.use(store)
 app.use(router)
 
